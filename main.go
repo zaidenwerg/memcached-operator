723fc31d3123f39adb3eae131bce0f4575187225
Uri Zaidenwerg <uri.zaidenwerg@replix.io>
2020-12-21 14:57:44 +0200

    fixed refernces to example non existent repo

--- main.go
+++ main.go
@@ -31,8 +31,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	cachev1alpha1 "github.com/example-inc/memcached-operator/api/v1alpha1"
-	"github.com/example-inc/memcached-operator/controllers"
+	cachev1alpha1 "github.com/zaidenwerg/memcached-operator/api/v1alpha1"
+	"github.com/zaidenwerg/memcached-operator/controllers"
 	// +kubebuilder:scaffold:imports
 )
 
